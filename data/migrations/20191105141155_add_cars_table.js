7d1ee9c1004786ce5f97e3ed2c1641a176e14390
Sheila Eichenberger <grandpoobah@Sheilas-MacBook-Pro.local>
2019-11-05 14:17:19 -0800

    migration folder built

--- data/migrations/20191105141155_add_cars_table.js
+++ data/migrations/20191105141155_add_cars_table.js
@@ -1,8 +1,20 @@
 
 exports.up = function(knex) {
-  
+    return knex.schema.createTable('cars', function(table) {
+        // adds a primary key, called 'id' as and auto-increment integer, not null and unique
+        table.increments();
+    
+        table.string('VIN', 128).notNullable();
+        table.string('make', 64).notNullable();
+        table.string('model', 64).notNullable();
+        table.float('milage', 255).notNullable();
+        table.string('titleStatus', 255);
+        table.string('transmissionType', 128);
+    
+        table.timestamps(true, true);
+      });
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema.dropTableIfExists('cars');
 };

8a7354e4a7b472b960a1697ddb9f8dc54d2190d1
Sheila Eichenberger <grandpoobah@Sheilas-MacBook-Pro.local>
2019-11-06 05:54:05 -0800

    added foriegn key and sales table

--- data/migrations/20191105141155_add_cars_table.js
+++ data/migrations/20191105141155_add_cars_table.js
@@ -1,17 +1,41 @@
 
 exports.up = function(knex) {
-    return knex.schema.createTable('cars', function(table) {
-        // adds a primary key, called 'id' as and auto-increment integer, not null and unique
-        table.increments();
-    
-        table.string('VIN', 128).notNullable();
-        table.string('make', 64).notNullable();
-        table.string('model', 64).notNullable();
-        table.float('milage', 255).notNullable();
-        table.string('titleStatus', 255);
-        table.string('transmissionType', 128);
-        table.timestamps(true, true);
-      });
+    return knex.schema
+        .createTable('cars', function(table) {
+            // adds a primary key, called 'id' as and auto-increment integer, not null and unique
+            table.increments();
+        
+            table.string('VIN', 128).notNullable();
+            table.string('make', 64).notNullable();
+            table.string('model', 64).notNullable();
+            table.float('milage', 255).notNullable();
+            table.string('titleStatus', 255);
+            table.string('transmissionType', 128);
+
+            table.timestamps(true, true);
+
+            table
+                .foreign('titleStatus')
+                .references('user_id')
+                .inTable('sales')
+        })
+        .createTable('sales', function(sales) {
+            sales.increments();
+            sales.('text').notNullable();
+      
+            sales
+              .integer('user_id')
+              .unsigned()
+              .notNullable()
+              .references('id')
+              .inTable('cars')
+              .onDelete('CASCADE')
+              .onUpdate('CASCADE');
+
+            sales.timestamps(true, true);
+
+
+          });
 };
 
 exports.down = function(knex) {
